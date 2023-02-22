bffccb0d91b0aa0baf1edcae27a2f906f66902e7
Ulukbec <ulukbek691@gmail.com>
2023-02-22 16:08:20 +0600

    modified

--- geeksfit/urls.py
+++ geeksfit/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('trainings/', views.TrainingModelViewSet.as_view()),
     path('trainings/<int:id>/', views.TrainingDetailView.as_view()),
-    path('favotiet/', views.FavoriteView.as_view())
 ]
