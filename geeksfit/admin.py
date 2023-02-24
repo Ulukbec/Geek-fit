5b6799c3bc65f8b036d7908b8912038c2fdb639f
Kutman <116498797+Kutman73@users.noreply.github.com>
2023-02-24 21:39:53 +0600

    Update admin.py
    added to admin

--- geeksfit/admin.py
+++ geeksfit/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Training
+from .models import Training, FavoriteTraining
 
 admin.site.register(Training)
-
+admin.site.register(FavoriteTraining)
