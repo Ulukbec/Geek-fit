b9686874c1095d799879c69d8938e909f2918b9b
Ulukbec <114999981+Ulukbec@users.noreply.github.com>
2023-03-14 19:34:47 +0600

    Update urls.py
    update

--- personal_area/urls.py
+++ personal_area/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('personal_inform_view/', views.PersonalInformAPIView.as_view()),
     path('personal_inform_edit/<int:id>/', views.PersonalInformRUAPIView.as_view()),
     path('my_card/', views.MyCardAPIView.as_view()),
+    path('profile_email/', views.EmailProfileView.as_view()),
     path('my_card/<int:id>/', views.MyCardRUAPIView.as_view()),
 ]
